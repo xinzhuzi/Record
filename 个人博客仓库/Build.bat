a6f266546cb2292b7360c12e88c59295b5cd99e7
郑冰津 <1487842110@qq.com>
2020-05-08 23:22:31 +0800

    修改个人博客数据

--- 个人博客仓库/Build.bat
+++
@@ -1,8 +0,0 @@
-cd %~dp0
-
-hexo clean
-hexo g
-hexo d
-
-REM 将 public 里面的静态站点数据全部 copy 到外层,GitHub 上面会展示出来的
-copy -rvf public/* ../
--- 个人博客仓库/Build.sh
+++ 个人博客仓库/Build.sh
@@ -2,7 +2,8 @@ cd "$(dirname "$0")"
 
 hexo clean
 hexo g
-hexo d
 
 # 将 public 里面的静态站点数据全部 copy 到外层,GitHub 上面会展示出来的
 cp -rvf public/* ../
+
+sleep 30s
