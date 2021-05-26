e699c3eaa69936826a74b1fedca3724c18dd35e8
xinzhuzi <1487842110@qq.com>
2021-05-26 10:24:14 +0800

    GC

--- Build.sh
+++ Build.sh
@@ -3,7 +3,11 @@ cd "$(dirname "$0")"
 hexo clean
 hexo g
 
+
 # 将 public 里面的静态站点数据全部 copy 到外层,GitHub 上面会展示出来的
 cp -rvf public/* ../xinzhuzi.github.io/
 
+find ../xinzhuzi.github.io/ -type f  -iname '*.pdf' -delete
+
 sleep 10s
+
